f70a603b1c495355db2abe8366d25666f2374b90
Bruno Carrard <brunocarrardoliveira@gmail.com>
2024-07-03 18:18:24 -0300

    adding dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from "pinia";
+import { createToastifyPlugin } from 'vue-toastify-3';
+import 'vue-toastify-3/style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
+app.use(router);
+app.use(createToastifyPlugin, {
+    autoClose: 5000,
+    closeOnClick: true,
+})
+app.mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    purge: ["./index.html", "./src/**/*/{vue, js, ts, jsx, tsx}"],
+    darkMode: false,
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: [],
+}
